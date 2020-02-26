8cd5129eb12b29ef9a631dc2714a12c8037705e3
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-26 08:41:35 -0800

    Added option to log move requests.

--- main.go
+++ main.go
@@ -10,14 +10,32 @@ import (
 )
 
 var startResp = api.StartResponse{Color: "#75CEDD", SecondaryColor: "#7A75DD"}
+var movelog *log.Logger
+var logging bool // Turns on logging JSON move data for replaying games
 
 func main() {
-	port := os.Getenv("PORT") // Get Heroku port
+	port := os.Getenv("PORT") // Get Heroku port (or non-Heroku port, I guess)
 
 	if port == "" {
 		log.Printf("$PORT not set, defaulting to 8080")
 		port = "8080"
 	}
+
+	logging = false
+	args := os.Args[1:]
+	if len(args) > 0 {
+		// Who needs argparse anyway
+		if args[0] == "log" {
+			log.Printf("Logging enabled")
+			logging = true
+		}
+	}
+
+	logfile, err := os.Open("/dev/null")
+	if err != nil {
+		panic(err)
+	}
+	movelog = log.New(logfile, "", 0)
 
 	http.HandleFunc("/start", util.LogRequest(start))
 	http.HandleFunc("/move", util.LogRequest(move))
--- routes.go
+++ routes.go
@@ -5,14 +5,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/otonnesen/battlesnake2020/api"
+	"github.com/otonnesen/battlesnake2020/logic"
 )
 
 func start(w http.ResponseWriter, req *http.Request) {
-	_, err := api.NewStartRequest(req) // TODO: Do something with data?
+	data, err := api.NewStartRequest(req)
 	if err != nil {
 		log.Printf("Bad start request: %v\n", err)
+	}
+	if logging {
+		fname := strconv.Itoa(data.GameID) + ".log"
+		logfile, err := os.Create(fname)
+		if err != nil {
+			panic(err)
+		}
+		movelog = log.New(logfile, "", 0)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(startResp)
@@ -20,7 +31,7 @@ func start(w http.ResponseWriter, req *http.Request) {
 
 func move(w http.ResponseWriter, req *http.Request) {
 	data, err := api.NewMoveRequest(req)
-	log.Printf("%+v\n", data)
+	// log.Printf("%+v\n", data)
 	if err != nil {
 		log.Printf("Bad move request: %v\n", err)
 		w.Header().Set("Content-Type", "application/json")
@@ -28,7 +39,14 @@ func move(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	resp := &api.MoveResponse{Move: "up"} // Get Move
+	move := logic.GetMove(data)
+	log.Printf("MOVE: %s\n", move)
+	if logging {
+		jsonstr, _ := json.MarshalIndent(data, "", "  ")
+		movelog.Printf("%s\n", jsonstr)
+	}
+
+	resp := &api.MoveResponse{Move: string(move)} // Get Move
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp)
