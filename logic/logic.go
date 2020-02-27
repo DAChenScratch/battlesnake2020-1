01e0d243ea98f3299e9e9ae46ce845056959db61
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-26 17:26:22 -0800

    Added heuristic function for use in minimax (or elsewhere).

--- logic/logic.go
+++ logic/logic.go
@@ -1,6 +1,10 @@
 package logic
 
-import "github.com/otonnesen/battlesnake2020/api"
+import (
+	"math"
+
+	"github.com/otonnesen/battlesnake2020/api"
+)
 
 type Direction string
 
@@ -14,4 +18,30 @@ const (
 func GetMove(data *api.MoveRequest) Direction {
 	// state := InitState(data)
 	return Up
+}
+
+func (s State) heuristic(snakeID string) float64 {
+	// TODO: Just makes you not die (sometimes) for now
+	you := s.Snakes[snakeID]
+	head := you.Head()
+	if s.outOfBounds(head) {
+		return math.Inf(-1)
+	}
+	if s.inSnakeBody(head) {
+		return math.Inf(-1)
+	}
+	return math.Inf(1)
+}
+
+func (s State) outOfBounds(p Point) bool {
+	return p.X < 0 || p.X >= s.Width || p.Y < 0 || p.Y >= s.Height
+}
+
+func (s State) inSnakeBody(p Point) bool {
+	for _, snake := range s.Snakes {
+		if p.In(snake.Body) {
+			return true
+		}
+	}
+	return false
 }
